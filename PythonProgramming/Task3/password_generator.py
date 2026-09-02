4227cce0323a0cea21ceea684b2fc4bcabb3f37f
Adarsh Gouda <goudaadarsh923@gmail.com>
2026-09-02 21:46:16 +0530

    Update password_generator.py

--- PythonProgramming/Task3/password_generator.py
+++ PythonProgramming/Task3/password_generator.py
@@ -1,1 +1,34 @@
+import random
+import string
 
+print("===== PASSWORD GENERATOR =====")
+
+while True:
+    try:
+        length = int(input("Enter password length: "))
+
+        if length < 4:
+            print("Password length must be at least 4.")
+            continue
+
+        characters = (
+            string.ascii_letters +
+            string.digits +
+            string.punctuation
+        )
+
+        password = ""
+
+        for i in range(length):
+            password += random.choice(characters)
+
+        print("\nGenerated Password:", password)
+
+        choice = input("\nGenerate another password? (yes/no): ").lower()
+
+        if choice != "yes":
+            print("Thank you for using Password Generator!")
+            break
+
+    except ValueError:
+        print("Please enter a valid number.")
